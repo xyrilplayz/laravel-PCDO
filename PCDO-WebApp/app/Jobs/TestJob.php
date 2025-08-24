a3c32e13ba8a6826ec6b22bcf182a71b9c834b77
Relova-John <170879264+Relova-John@users.noreply.github.com>
2025-08-24 23:37:54 +0800

    Redis Jobs Cache Saved

---
+++ PCDO-WebApp/app/Jobs/TestJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class TestJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        file_put_contents(storage_path('logs/job.log'), $this->message.PHP_EOL, FILE_APPEND);
+    }
+}
--- PCDO-WebApp/routes/web.php
+++ PCDO-WebApp/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Jobs\TestJob;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dispatch-test', function() {
+    TestJob::dispatch('Hello Redis Queue!');
+    return 'Job dispatched!';
 });
